a12ffff75d41420333e2c8ce7b9bcb2ef270b573
SomeRandomPizzaGuy <edvin.gjervaldsaeter@falun.ntig.se>
2018-01-19 10:06:35 +0100

    Multiplikationstabellen
    Första utmaningen

---
+++ Utmaning1_Multiplikationstabellen_Edvin/Utmaning1_Multiplikationstabellen_Edvin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utmaning1_Multiplikationstabellen_Edvin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skriver ut en hel rad och hoppar ned
+            Console.WriteLine("Multiplikationstabeller");
+
+            // Skriver ut en text men som inte hoppar ur
+            Console.Write("Skriv in ett valfrit heltal:");
+
+            // Denna kod gör så att man kan mata in tal och bokstäver
+            int Tal1 = int.Parse(Console.ReadLine());
+
+            // Simpel for loop som gångrar det inmatade talet från 1 till 10
+            for (int Tal2 = 1; Tal2 < 11; Tal2++)
+            {
+                Console.WriteLine(Tal1 * Tal2);
+            }
+
+            // Stoppar programmet från att stänga ned sig automatiskt när koden har körts
+            Console.ReadLine();
+        }
+    }
+}
